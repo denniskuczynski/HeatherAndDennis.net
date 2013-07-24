4ebb8b811fbb5af06c4ce0d68e7f9b475cb74032
denniskuczynski <dennis.kuczynski@gmail.com>
2013-07-23 21:42:58 -0400

    First attempt using jquery_file_upload

--- app/heather_and_dennis.rb
+++ app/heather_and_dennis.rb
@@ -1,5 +1,7 @@
 require 'rubygems'
 require 'sinatra/base'
+require 'base64'
+require 'openssl'
 require 'json'
 require 'erb'
 
--- app/heather_and_dennis/server.rb
+++ app/heather_and_dennis/server.rb
@@ -48,8 +48,43 @@ module HeatherAndDennis
       erb :registry
     end
 
+    get "/signed_urls" do
+      content_type :json
+      { policy: s3_upload_policy_document,
+        signature: s3_upload_signature,
+        key: "uploads/#{SecureRandom.uuid}/#{params[:doc][:title]}",
+        success_action_redirect: "/"
+      }.to_json
+    end
+
     not_found do
       redirect '/404.html'
+    end
+
+    private
+
+    def s3_upload_policy_document
+      Base64.encode64(
+        {
+          expiration: (Time.now + (30*60)).utc.strftime('%Y-%m-%dT%H:%M:%S.000Z'),
+          conditions: [
+            { bucket: ENV['S3_BUCKET'] },
+            { acl: 'public-read' },
+            ["starts-with", "$key", "uploads/"],
+            { success_action_status: '201' }
+          ]
+        }.to_json
+      ).gsub(/\n|\r/, '')
+    end
+
+    def s3_upload_signature
+      Base64.encode64(
+        OpenSSL::HMAC.digest(
+          OpenSSL::Digest::Digest.new('sha1'),
+          ENV['AWS_SECRET_KEY_ID'],
+          s3_upload_policy_document
+        )
+      ).gsub(/\n/, '')
     end
     
   end
